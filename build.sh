e05c8ec5336ed24097414a8a7ecc4c257e124b1c
Ali Malekpour <542968+malekpour@users.noreply.github.com>
2025-03-26 19:23:23 -0400

    Update U-Boot configuration for NanoPC-T6 and add Nix flake support

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PATH=$(echo "$PATH" | tr ':' '\n' | grep -v '^/mnt/c/' | paste -sd:)
 
-BR_PATH="$(pwd)/.buildroot"
+BR_PATH="$(pwd)/../.buildroot"
 EXTERNAL_PATH="$(pwd)"
 cd "$BR_PATH"
 make BR2_EXTERNAL="$EXTERNAL_PATH" "$@"
---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        crossPkgs = pkgs.pkgsCross.aarch64-multiplatform;
+      in {
+        devShell = pkgs.mkShellNoCC {
+          buildInputs = with pkgs; [
+            # tools
+            gcc
+
+            # AArch64 cross toolchain (binaries: aarch64-unknown-linux-gnu-gcc, etc.)
+            crossPkgs.buildPackages.binutils
+            crossPkgs.buildPackages.gcc
+          ];
+
+          shellHook = ''
+            export PS1="[🏗️ buildroot] \u@\h:\w\$ "
+          '';
+        };
+      }
+    );
+}
